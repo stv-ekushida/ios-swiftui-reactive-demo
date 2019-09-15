d561ddeb77295d883180f1700a21af1fb22cd265
stv-ekushida <e.kushida@st-ventures.jp>
2019-09-16 07:47:09 +0900

    Initial Commit

---
+++ swift-ui-reactive-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-ui-reactive-demo
+//
+//  Created by Kushida　Eiji on 2019/09/16.
+//  Copyright © 2019 Kushida　Eiji. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-ui-reactive-demoTests/swift_ui_reactive_demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  swift_ui_reactive_demoTests.swift
+//  swift-ui-reactive-demoTests
+//
+//  Created by Kushida　Eiji on 2019/09/16.
+//  Copyright © 2019 Kushida　Eiji. All rights reserved.
+//
+
+import XCTest
+@testable import swift_ui_reactive_demo
+
+class swift_ui_reactive_demoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
